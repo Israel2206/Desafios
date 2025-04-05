8fc3614b6a7f996ce65541519726c48962ecf263
Israel <brenoisrael931@gmail.com>
2025-04-05 20:20:11 -0300

    Exercicio de Fatorial

---
+++ poo/src/Fatorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int mult = 1;
+        System.out.print("Digite o numero pra fatorar: ");
+        int fatorial = scanner.nextInt();
+
+        for(int contador = 1; contador <=fatorial; contador++ ){
+            mult = mult *contador;
+        }
+        System.out.println(mult);
+        scanner.close();
+    }
+}
