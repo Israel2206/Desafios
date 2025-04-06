a9c9ad8211699aaaf5fb89e943245769d6fcdf1d
Israel <brenoisrael931@gmail.com>
2025-04-06 10:13:09 -0300

    imprimir os numeros decrecente

---
+++ poo/src/ContagemRegressiva.java
@@ -0,0 +1,16 @@
+// Imprima os números de 10 a 1.
+import java.util.Scanner;
+
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int numero = scanner.nextInt();
+
+        for(int i = 0; i<numero; numero--){
+            System.out.println(numero);
+        }
+        scanner.close();
+    }
+}
