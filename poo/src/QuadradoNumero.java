5e7218909d50c62a5f35fc1c5f084db15e0db9cc
Israel <brenoisrael931@gmail.com>
2025-04-06 11:04:27 -0300

    indo ate  quadrado de um numero

---
+++ poo/src/QuadradoNumero.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class QuadradoNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int numero = scanner.nextInt();
+        for(int contador = 1; contador <=numero; contador++){
+            System.out.println("O quadrado de "+contador+" é "+contador*contador);
+        }
+        scanner.close();
+    }
+}
