772af8812bd76e9127c952903b407bd5a292d7cd
Israel <brenoisrael931@gmail.com>
2025-04-06 22:49:53 -0300

    multiplos de tres e cinco

---
+++ poo/src/Multiplos.java
@@ -0,0 +1,15 @@
+// Mostre todos os múltiplos de 3 e 5 entre 1 e 100.
+public class Multiplos {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <=100) {
+            if (numero %3==0) {
+                System.out.println(numero);
+            }else if (numero%5==0) {
+                System.out.println(numero);
+            }
+            numero++;
+        }
+    }
+}
