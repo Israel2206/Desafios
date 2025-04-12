d11a3d578676b594b48ace08ec4f8b485d8fd51d
Israel <brenoisrael931@gmail.com>
2025-04-11 21:03:05 -0300

    Produto

---
+++ poo/src/Desconto.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Desconto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o preço do produto: ");
+        int preco = scanner.nextInt();
+
+        System.out.print("digite o desconto: ");
+        int desconto = scanner.nextInt();
+
+        Produto produto = new Produto(preco);
+
+        int precoFinal = produto.calcular_desconto(desconto);
+
+        System.out.println("Preço final com desconto: " + precoFinal);
+
+        scanner.close();
+
+    }
+
+}
---
+++ poo/src/Produto.java
@@ -0,0 +1,12 @@
+public class Produto {
+    private int preco;
+   
+    public Produto(int preco){
+        this.preco = preco;
+    }
+
+    public int calcular_desconto(int desconto){
+        return preco - desconto;
+    }
+
+}
