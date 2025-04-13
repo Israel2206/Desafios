c74aca89239493d9588013de88f2f70ce98995b1
Israel <brenoisrael931@gmail.com>
2025-04-13 14:59:42 -0300

    desafios de logicas

---
+++ poo/src/logica02/SomaAlternada.java
@@ -0,0 +1,21 @@
+package logica02;
+import java.util.Scanner;
+
+public class SomaAlternada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int soma = 0;
+        int numero = scanner.nextInt();
+        for(int contador = 0; contador < numero; contador++){
+            int valor = scanner.nextInt();
+            if (contador%2==0) {
+                soma+=valor;
+            }else{
+                soma-=valor;
+            }
+        }
+        System.out.println(soma);
+        scanner.close();
+    }
+}
