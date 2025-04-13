e336d6aa387947ff7a5154e5edd3cd44b3053982
Israel <brenoisrael931@gmail.com>
2025-04-13 13:43:39 -0300

    POO

--- poo/src/instaciamento/carro/cachorro/Cachorro.java
+++ poo/src/instaciamento/carro/cachorro/Cachorro.java
@@ -1,3 +1,4 @@
+package instaciamento.carro.cachorro;
 public class Cachorro {
     String nome;
     String raca;
--- poo/src/instaciamento/carro/cachorro/Main.java
+++ poo/src/instaciamento/carro/cachorro/Main.java
@@ -1,7 +1,8 @@
+package instaciamento.carro.cachorro;
 public class Main {
     public static void main(String[] args) {
         Cachorro cachorro1 = new Cachorro("Hulk", "vira lata", 3);
-        Cachorro cachorro2 = new Cachorro("bella", "Pastor alemão", 1);
+        Cachorro cachorro2 = new Cachorro("Bella", "Pastor Alemão", 1);
 
         cachorro1.exibirCachorro();
         cachorro2.exibirCachorro();
