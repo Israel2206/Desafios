fc633801f18e4470089f47f727f8828ffff89ec8
Israel <brenoisrael931@gmail.com>
2025-04-13 14:30:14 -0300

    POO

---
+++ poo/src/instaciamento/pessoa/Main.java
@@ -0,0 +1,11 @@
+package instaciamento.pessoa;
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Israel");
+        Pessoa pessoa2 = new Pessoa("Leleo");
+        Pessoa pessoa3 = new Pessoa("Mari");
+
+        pessoa1.apresentar(pessoa2);
+        pessoa2.apresentar(pessoa3);
+    }
+}
