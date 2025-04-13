005e84020575ab94d095eeb0d421f0fbc9dc5b57
Israel <brenoisrael931@gmail.com>
2025-04-13 15:23:47 -0300

    logica de progamação

---
+++ poo/src/logica02/PontoMedio.java
@@ -0,0 +1,20 @@
+package logica02;
+import java.util.Scanner;
+
+public class PontoMedio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+
+        double xm = (x1 + x2) / 2.0;
+        double ym = (y1 + y2) / 2.0;
+
+        System.out.printf("%.2f %.2f", xm, ym);
+
+        scanner.close();
+    }
+}
