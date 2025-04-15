00348f47d87da75243e44fbe2a4ffded743e4f33
Israel <brenoisrael931@gmail.com>
2025-04-14 22:28:29 -0300

    exercicio de encapsulamento

---
+++ poo/src/encapsulamento/produto/Main.java
@@ -0,0 +1,14 @@
+package encapsulamento.produto;
+public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto();
+
+        produto1.setNome("caneta");
+        produto1.setPreco(2.50);
+        produto1.setQuantidade(100);
+
+        System.out.println("Produto: "+produto1.getNome());
+        System.out.printf("Preço: R$%.2f%n",produto1.getPreco());
+        System.out.println("Quantidade: "+produto1.getQuantidade());
+    }
+}
