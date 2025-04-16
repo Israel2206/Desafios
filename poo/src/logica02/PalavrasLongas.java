6376ad8251d6622a8116e139937ea06973a93226
Israel <brenoisrael931@gmail.com>
2025-04-15 22:23:12 -0300

    logica de progamação

---
+++ poo/src/logica02/PalavrasLongas.java
@@ -0,0 +1,22 @@
+package logica02;
+import java.util.Scanner;
+
+public class PalavrasLongas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i = 1;i<=num;i++){
+            String n = scanner.nextLine();
+            if (n.length() > 10) {
+                int l = n.length();
+                System.out.println(n.charAt(0)+String.valueOf(l-2)+n.charAt(l-1));
+            }else{
+                System.out.println(n);
+            }
+        }
+        scanner.close();
+    }
+}
