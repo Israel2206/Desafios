1a9b78ba95a03930a347345f2e5a5fc338ae7f13
Israel <brenoisrael931@gmail.com>
2025-04-17 20:44:44 -0300

    exercicio de logica de programação

---
+++ poo/src/logica03/Triagulo.java
@@ -0,0 +1,21 @@
+package logica03;
+import java.util.Scanner;
+
+public class Triagulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double C = scanner.nextDouble();
+
+        if ((A < B + C) && (B < A + C) && (C < A + B)) {
+            double perimetro = A + B + C;
+            System.out.println("Perimetro = "+perimetro);
+        }else{
+            double area = ((A + B) * C) / 2;
+            System.out.println("Area = "+area);
+        }
+        scanner.close();
+    }
+}
