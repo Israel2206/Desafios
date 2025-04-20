7bf8dd6a1f4a751471dcf92f6d2a1e7d926576ea
Israel <brenoisrael931@gmail.com>
2025-04-20 20:22:36 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/Senha.java
@@ -0,0 +1,20 @@
+package logica05;
+import java.util.Scanner;
+
+public class Senha {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int senha_real = 2002;
+
+        while (true) {
+            int senha = scanner.nextInt();
+            if (senha == senha_real) {
+                System.out.println("Acesso Permitido");
+                break;
+            }else{
+                System.out.println("Senha Invalida");
+            }
+        }
+        scanner.close();
+    }
+}
