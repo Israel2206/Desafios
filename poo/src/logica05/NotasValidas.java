1bb1fa3e1001136c3dab488317d72c091015029c
Israel <brenoisrael931@gmail.com>
2025-04-20 20:29:59 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/NotasValidas.java
@@ -0,0 +1,26 @@
+package logica05;
+import java.util.Scanner;
+
+public class NotasValidas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int quant = 0;
+        double soma = 0;
+        while (true) {
+            double nota = scanner.nextDouble();
+            if (nota >=0 && nota <=10) {
+                soma += nota;
+                quant++;
+                if (quant ==2) {
+                    break;
+                }
+            }else{
+                System.out.println("nota invalida");
+            }
+        }
+        double media = soma/2;
+        System.out.printf("media = %.2f%n",media);
+        scanner.close();
+    }
+}
