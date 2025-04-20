a49aa28fde7f66e63fcff9a3e24afdb5a737e0a8
Israel <brenoisrael931@gmail.com>
2025-04-20 15:27:24 -0300

    exercicio de logica de programação

---
+++ poo/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int in = 0;
+        int out = 0;
+
+        int n = scanner.nextInt();
+        for(int i = 1; i <= n ; i++){
+            int N = scanner.nextInt();
+            if (N >= 10 && N <=20) {
+                in++;
+            }else{
+                out++;
+            }
+        }
+        System.out.println(in+" in");
+        System.out.println(out+" out");
+        scanner.close();
+    }
+}
---
+++ poo/src/logica04/QuadradoPares.java
@@ -0,0 +1,17 @@
+package logica04;
+import java.util.Scanner;
+
+public class QuadradoPares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for(int i = 1; i <= N ; i++){
+            if (i%2==0) {
+                System.out.println(i+"^"+"2"+" = "+(i*i));
+            }
+        }
+        scanner.close();
+    }
+}
