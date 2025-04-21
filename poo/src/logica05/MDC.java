4db8233e7aa601850dbc3e9a90e1be4b6d8738d2
Israel <brenoisrael931@gmail.com>
2025-04-21 11:25:15 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/MDC.java
@@ -0,0 +1,29 @@
+package logica05;
+import java.util.Scanner;
+
+public class MDC {
+    // Função para calcular o MDC usando o algoritmo de Euclides
+    public static int mdc(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int n1 = scanner.nextInt();
+            int n2 = scanner.nextInt();
+
+            int resultado = mdc(n1, n2);
+            System.out.println(resultado);
+        }
+
+        scanner.close();
+    }
+}
