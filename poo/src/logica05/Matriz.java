b8d86257671cbb3d4e548cea8d63ec4295480bf0
Israel <brenoisrael931@gmail.com>
2025-04-24 20:47:15 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/Matriz.java
@@ -0,0 +1,13 @@
+package logica05;
+public class Matriz {
+    public static void main(String[] args) {
+        int [][] numeros = {{1,2,3},{40,50,60,},{700,800,900}};
+
+        for(int l = 0; l < numeros.length; l++){
+            for(int c = 0; c < numeros[l].length ; c++){
+                System.out.printf("[%d]",numeros[l][c]);
+            }
+            System.out.printf("%n");
+        }
+    }
+}
