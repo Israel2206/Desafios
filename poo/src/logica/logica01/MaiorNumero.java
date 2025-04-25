ec8789fe874ae7beb781c4080e7c0621c97215c8
Israel <brenoisrael931@gmail.com>
2025-04-24 22:19:48 -0300

    exercicio de logica de programação

---
+++ poo/src/logica/logica01/MaiorNumero.java
@@ -0,0 +1,34 @@
+package logica.logica01;
+import java.util.Scanner;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matriz = new int[3][3];
+
+        int soma = 0;
+
+        int maior = 0;
+        int linha = 0;
+        int coluna = 0;
+
+        for(int l = 0; l < matriz.length; l++){
+            for(int c = 0; c < matriz[l].length ; c++){
+                System.out.printf("Digite o número da posição [%d][%d]: ", l, c);
+                matriz[l][c] = scanner.nextInt();
+            }
+        }
+        for(int l = 0; l < matriz.length; l++){
+            for(int c = 0; c < matriz[l].length ; c++){
+                if (matriz[l][c] > maior) {
+                    maior = matriz[l][c];
+                    linha = l;
+                    coluna = c;
+                }
+            }
+        }
+        System.out.printf("[%d]%n",maior);
+        System.out.println("linha: "+linha+" \ncoluna: "+coluna);
+    }
+}
