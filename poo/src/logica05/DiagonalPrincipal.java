f2229da9c4367e857b1a8c8ed490783f64da17c0
Israel <brenoisrael931@gmail.com>
2025-04-24 22:04:11 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/DiagonalPrincipal.java
@@ -0,0 +1,26 @@
+package logica05;
+import java.util.Scanner;
+
+public class DiagonalPrincipal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matriz = new int[3][3];
+
+        int soma = 0;
+
+        for(int l = 0; l < matriz.length; l++){
+            for(int c = 0; c < matriz[l].length ; c++){
+                System.out.printf("Digite o número da posição [%d][%d]: ", l, c);
+                matriz[l][c] = scanner.nextInt();
+            }
+        }
+        for(int l = 0; l < matriz.length; l++){
+            for(int c = 0; c < matriz[l].length ; c++){
+                if (l == c) {
+                    System.out.printf("[%d]",matriz[l][c]);
+                }
+            }
+        }
+    }
+}
