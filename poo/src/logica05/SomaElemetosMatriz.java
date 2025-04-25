965f35bf92883e727decdcfdccb973605b949c46
Israel <brenoisrael931@gmail.com>
2025-04-24 21:27:34 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/SomaElemetosMatriz.java
@@ -0,0 +1,18 @@
+package logica05;
+public class SomaElemetosMatriz {
+    public static void main(String[] args) {
+        int[][] numeros = {
+            {1, 2},
+            {3, 4}
+        };
+
+        int soma = 0;
+
+        for(int l = 0 ; l < numeros.length ; l++){
+            for(int c = 0; c <numeros[l].length; c++){
+                soma += numeros[l][c];
+            }
+        }
+        System.out.println("A soma dos elementos é: " + soma);
+    }
+}
