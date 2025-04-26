5805be928f1641e07361487b1606915c9e9dfad3
Israel <brenoisrael931@gmail.com>
2025-04-26 14:49:08 -0300

    exercicio de logica de programação

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int[][] matriz = new int[3][3];
+        int[][] matriz = new int[3][4];
+
+        int soma = 0;
 
         for(int l =0; l < matriz.length; l++){
             for(int c = 0; c < matriz[l].length; c++){
                 matriz[l][c]=scanner.nextInt();
+
             }
         }
         for(int l =0; l < matriz.length; l++){
@@ -17,6 +20,16 @@ public class Main {
             }
             System.out.printf("%n");
         }
+
+        System.out.println("A soma de cada linha: ");
+        for(int l =0; l < matriz.length; l++){
+            for(int c = 0; c < matriz[l].length; c++){
+                soma+=matriz[l][c];
+            }
+            System.out.printf("[%d] ", soma);
+            soma = 0;
+        }
+
         scanner.close();
     }
 }
