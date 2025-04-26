81ca136a2a2f3f4a57db4c8366d1b08b51c589ff
Israel <brenoisrael931@gmail.com>
2025-04-26 15:00:57 -0300

    exercicio de logica de peorgramação

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int[][] matriz = new int[4][4];
-
+        int[][] matriz = new int[2][3];
 
         for(int l =0; l < matriz.length; l++){
             for(int c = 0; c < matriz[l].length; c++){
@@ -13,7 +12,21 @@ public class Main {
 
             }
         }
+        System.out.println("Original: ");
+        for(int l =0; l < matriz.length; l++){
+            for(int c = 0; c < matriz[l].length; c++){
+                System.out.printf("[%d]", matriz[l][c]);
+            }
+            System.out.printf("%n");
+        }
 
+        System.out.println("\nTransposta: ");
+        for(int c = 0; c < matriz[0].length; c++){
+            for(int l = 0; l < matriz.length; l++){
+                System.out.printf("[%d]", matriz[l][c]);
+            }
+            System.out.printf("%n");
+        }
         scanner.close();
     }
 }
