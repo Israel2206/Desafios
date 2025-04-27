409f121eecc621b0d54572d76cdaf79e0ee72cab
Israel <brenoisrael931@gmail.com>
2025-04-27 14:41:40 -0300

    exercicio de logica de programação

---
+++ poo/src/logica05/SobrinhodoMeio.java
@@ -0,0 +1,20 @@
+package logica05;
+import java.util.Scanner;
+
+public class SobrinhodoMeio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int h = scanner.nextInt();
+        int z = scanner.nextInt();
+        int l = scanner.nextInt();
+
+        if ((h > z && h < l) || (h < z && h > l)) {
+            System.out.println("huguinho");
+        } else if ((z > h && z < l) || (z < h && z > l)) {
+            System.out.println("zezinho");
+        } else {
+            System.out.println("luisinho");
+        }
+    }
+}
