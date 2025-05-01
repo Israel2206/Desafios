46b9e15d01627c1b1a53ea21ff0f6dc0ce7bc0ba
Isarel <brenoisrael931@gmail.com>
2025-05-01 14:05:47 -0300

    exercicio de logica de programação

--- logica/src/logica/logica05/Zero.java
+++ logica/src/logica/logica05/Zero.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class zero{
+public class Zero{
     public static void main(String[] args){
 		ArrayList<String> numeros = new ArrayList<>();
 		Scanner scanner = new Scanner(System.in);
