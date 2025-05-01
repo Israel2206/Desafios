5b26b22125bcdf7f1bdbff149ca2b64de925d8b8
Isarel <brenoisrael931@gmail.com>
2025-05-01 13:51:12 -0300

    exercicio de logica de programação

---
+++ logica/src/Zero.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Zero{
+    public static void main(String[] args){
+		ArrayList<String> numeros = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+
+		int soma = 0;
+
+		int chefe = scanner.nextInt();
+		scanner.nextLine();
+
+		for(int i = 1; i <= chefe; i++){
+			String numero = scanner.nextLine();
+
+			if(numero.equals("0")){
+				numeros.remove(numeros.size() -1);
+			}else{
+				numeros.add(numero);
+			}
+		}
+		for(int i = 0; i < numeros.size() ; i++){
+			int num = Integer.parseInt(numeros.get(i));
+			soma+=num;
+		}
+		System.out.println(soma);
+		scanner.close();
+    }
+}
